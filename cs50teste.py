5d4673a12c4056073543bb05044bc9ec3b73421d
dcassiani <dcassiani@gmail.com>
2025-04-29 11:46:53 -0300

    carga inicial

---
+++ cs50teste.py
@@ -0,0 +1,7 @@
+# 
+# python.exe -m pip install --upgrade pip
+# pip install pomegranate numpy scipy langchain scikit-learn
+# https://archive.org/details/python-data-science-handbook.pdf/page/44/mode/2up >> NumPy
+#
+
+print("CS50 AI Fundamentals sandbox")
---
+++ hello.py
@@ -0,0 +1,7 @@
+# python -m venv venv
+# .\venv\Scripts\activate
+# 
+# python.exe -m pip install --upgrade pip
+# 
+
+print("hello")
